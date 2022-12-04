4c23676253bb9742259be79a36febf091748abc8
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2022-12-05 00:44:58 +0100

    Fixed recipe logic and item ghosts

--- Assets/Modules/Interactions/Scripts/AltarInteractable.cs
+++ Assets/Modules/Interactions/Scripts/AltarInteractable.cs
@@ -11,12 +11,23 @@ namespace Potions.Gameplay
         protected override bool CanHolderInteract(ItemHolder holder) => holder.ItemId != null;
 
         protected override string GetItem() => null;
-        
-        protected override void OnItemAdded(string id) => ItemAdded?.Invoke(id);
+
+        protected override void OnItemAdded(string id)
+        {
+            ItemAdded?.Invoke(id);
+            var ghost = Instantiate(_itemGhostPrefab, _ghostOrigin);
+            ghost.transform.localPosition = Vector3.zero;
+            ghost.Setup(id);
+        }
 
         public override bool CanSkip(Interactor interactor)
         {
             return !CanInteract(interactor);
         }
+
+        [SerializeField]
+        private ItemGhost _itemGhostPrefab;
+        [SerializeField]
+        private Transform _ghostOrigin;
     }
 }
