a9312d6b31cecfbac2b8debe2a568d5c953bd100
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2022-12-11 13:13:42 +0100

    Bump animation adjustments

--- Assets/Modules/Interactions/Scripts/AnimationHelper.cs
+++ Assets/Modules/Interactions/Scripts/AnimationHelper.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Potions.Gameplay
@@ -15,12 +12,14 @@ namespace Potions.Gameplay
         public void Bump()
         {
             LeanTween.cancel(gameObject);
-            transform.localScale = new Vector3(_defaultScale.x * 1.1f, _defaultScale.y * 0.9f);
+            transform.localScale = new Vector3(_defaultScale.x * 1.1f * _multiplier, _defaultScale.y * 0.9f * _multiplier);
             LeanTween
                 .scale(gameObject, _defaultScale, 0.14f)
                 .setEaseInQuad();
         }
-        
+
+        [SerializeField]
+        private float _multiplier = 1f;
         private Vector3 _defaultScale;
     }
 }
